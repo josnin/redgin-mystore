a365d4e03656af6acf1d5256e0627970d04e5558
josnin <nino.capati@gmail.com>
2023-02-01 21:42:31 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -5,11 +5,14 @@ import { style2 } from './style2.css';
 import('redgin')
   .then(({ injectStyles }) => {
 
+    const styles = [
+      `@import url("https://fonts.googleapis.com/icon?family=Material+Icons")`, 
+      style2
+    ]
+
     // inject global styles
-    injectStyles.push(
-      `@import url("https://fonts.googleapis.com/icon?family=Material+Icons")`
-    )
-    injectStyles.push(style2)
+    injectStyles.push(...styles)
+   
 
     // load compon
     import('./src/layout/layout')
