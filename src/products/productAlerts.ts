123c4bb0de029e3fdaf4e513959886c70139c097
josnin <nino.capati@gmail.com>
2023-02-01 22:49:32 +0800

    cleanup

--- src/products/productAlerts.ts
+++ src/products/productAlerts.ts
@@ -5,11 +5,14 @@ class ProductAlerts extends RedGin {
   product = propReflect<Product>(null)
 
   static observedAttributes = ['product']
+  
   render() {
     return `
       ${ this.product && this.product.price > 700 ? ` 
         <p>          
-          <button ${ event('click', () => emit.call(this, 'notifyMe') ) } >Notify Me</button>
+          <button 
+            ${ event('click', () => emit.call(this, 'notifyMe') ) } 
+          >Notify Me</button>
         </p>      
       ` : `` }
 
